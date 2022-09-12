6d85e35d0435f57453b4a9e8b0f43ed60d618dec
Estevam <estevamfurtado@gmail.com>
2022-09-12 15:44:28 -0300

    commenting

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,20 +1,20 @@
 import Prisma from '@prisma/client';
-import parser from '../src/utils/parsers/index.js';
-import paths from './seed/files/paths.js';
-import data from './seed/data/index.js';
-import services from '../src/services/index.js';
+// import parser from '../src/utils/parsers/index.js';
+// import paths from './seed/files/paths.js';
+// import data from './seed/data/index.js';
+// import services from '../src/services/index.js';
 
-const { deputados, states, parties, rankings, records } = data;
+// const { deputados, states, parties, rankings, records } = data;
 
 const prisma = new Prisma.PrismaClient();
 
 async function main() {
 
-    console.log('>> Creating States...');
-    await services.state.createMany(states);
+    // console.log('>> Creating States...');
+    // await services.state.createMany(states);
 
-    console.log('>> Creating Parties...');
-    await services.party.createMany(parties);
+    // console.log('>> Creating Parties...');
+    // await services.party.createMany(parties);
 
     // console.log('>> Creating Politicians...');
     // await services.person.create.manyEach(deputados);
